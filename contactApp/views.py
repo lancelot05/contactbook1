75a39643b1b1f0d33250e1bb73eb59e616b15144
lancelot05 <53702551+lancelot05@users.noreply.github.com>
2019-12-21 20:07:13 +0530

    avoided repeated names of contacts

--- contactApp/views.py
+++ contactApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import ContactInfo
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -14,6 +15,14 @@ def add_contact(request):
     added_name = request.POST.get("contactName")
     added_number = request.POST.get("contactNumber")
     added_email = request.POST.get("contactEmail")
+
+    contacts = ContactInfo.objects.all()
+
+    for contact in contacts:
+        if contact.name == added_name:
+            messages.info(request, 'The Contact Name already exists! Try something different.')
+            return HttpResponseRedirect("/")
+
     created_contact = ContactInfo.objects.create(name = added_name, number = added_number, email = added_email)
 
     return HttpResponseRedirect("/")
